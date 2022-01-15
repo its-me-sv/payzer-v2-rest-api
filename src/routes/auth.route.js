2381757ec79a3b6a6ce92d64abb152b240569589
Suraj Vijay <surajcpp115@gmail.com>
2022-01-15 16:32:59 +0530

    verifying user

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,22 @@
 const router = require("express").Router();
 const db = require('../configs/postgres.config');
 
+router.post("/verify", async (req, res) => {
+    const { phoneNo } = req.body;
+    
+    const QUERY = `SELECT * FROM users WHERE phone_no = $1`;
+    const VALUE = [phoneNo];
+
+    try {
+        const { rowCount, rows } = await db.query(QUERY, VALUE);
+        if (!rowCount)
+            return res.status(200).json("No account");
+        const { otp, ...user } = rows[0];
+        return res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+});
 
 module.exports = router;
