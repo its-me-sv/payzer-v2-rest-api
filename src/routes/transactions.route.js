60a499f028c9c787b46f6eacac0e1b9061282e66
Suraj Vijay <surajcpp115@gmail.com>
2022-01-17 14:50:02 +0530

    retrieve transactions of user route

--- src/routes/transactions.route.js
+++ src/routes/transactions.route.js
@@ -15,4 +15,18 @@ router.post("/card", async (req, res) => {
     }
 });
 
+router.post("/user", async (req, res) => {
+    const { user_id } = req.body;
+
+    const QUERY = `SELECT * FROM get_user_transactions($1)`;
+    const VALUE = [user_id];
+
+    try {
+        const { rows } = await db.query(QUERY, VALUE);
+        return res.status(200).json(rows);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
 module.exports = router;
