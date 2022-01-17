73e3cfbae73fc2b45e408ce401dae51885537e13
Suraj Vijay <surajcpp115@gmail.com>
2022-01-17 12:31:52 +0530

    user retrieving route

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const bcrypt = require("bcrypt");
 const db = require("../configs/postgres.config");
 
+const mapIdentifier = {
+    user_id: "id",
+    phoneNo: "phone_no"
+};
+
 router.post("/create", async (req, res) => {
     try {
         const salt = await bcrypt.genSalt(+process.env.SALT);
@@ -31,6 +36,23 @@ router.post("/create", async (req, res) => {
         return res.status(500).json((err.detail && {error: err.detail}) || {error: true});
     }
 
+});
+
+router.post("/retrieve", async (req, res) => {
+    const { identifier, value } = req.body;
+    const column = mapIdentifier[identifier];
+    
+    const QUERY = `SELECT * FROM users WHERE ${column} = $1`;
+    const VALUE = [value];
+    
+    try {
+        const { rows, rowCount } = await db.query(QUERY, VALUE);
+        if (!rowCount) return res.status(400).json("No user");
+        const { otp, ...user } = rows[0]
+        return res.status(200).json(user);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
 });
 
 module.exports = router;
