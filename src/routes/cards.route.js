b43a64b1c79f8872c314433417ddfc0ba0bbbd0f
Suraj Vijay <surajcpp115@gmail.com>
2022-01-17 12:49:15 +0530

    user cards retrieving route

--- src/routes/cards.route.js
+++ src/routes/cards.route.js
@@ -1,3 +1,19 @@
 const router = require("express").Router();
+const db = require('../configs/postgres.config');
+
+router.post("/retrieve-all", async(req, res) => {
+    const { user_id } = req.body;
+
+    const QUERY = `SELECT id, is_primary, credit, debit, created_at  FROM get_user_cards($1)`;
+    const VALUE = [user_id];
+
+    try {
+        const { rows, rowCount } = await db.query(QUERY, VALUE);
+        if (!rowCount) return res.status(400).json("No user");
+        return res.status(200).json(rows);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
 
 module.exports = router;
