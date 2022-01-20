c4afb7b21af362054d4896929e13a6a68ce24662
Suraj Vijay <surajcpp115@gmail.com>
2022-01-20 06:57:47 +0530

    addded all possible case for port and explicitly added file name for scripts

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/validation', async (req, res) => {
     return res.status(200).json("validation route");
 });
 
-const PORT = process.env.port || process.env.PORT || 5000;
+const PORT = process.env.port || process.env.PORT || process.env.Port || 5000;
 app.listen(PORT, async () => {
     console.clear();
     console.log(`[REST API] Listening to PORT ${PORT}`);
