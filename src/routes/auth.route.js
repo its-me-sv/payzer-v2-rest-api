8f0a0aa3e572dd0ca148531a54219abef9162bcc
Suraj Vijay <surajcpp115@gmail.com>
2022-02-03 07:06:55 +0530

    added newUser val at /verify

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -22,13 +22,13 @@ router.post("/verify", async (req, res) => {
         const rows = await knex.select('*').from('users').where({ phone_no: phoneNo });
         const rowCount = rows.length;
         if (!rowCount)
-            return res.status(400).json("No account");
+            return res.status(200).json({newAccount: true});
         const { otp, ...user } = rows[0];
         const isLogged = await isUserLoggedIn(user.id);
         if (isLogged)
             return res.status(400).json("Already logged in");
         const jwt_token = await generateAccessToken(user);
-        return res.status(200).json({ user, jwt_token });
+        return res.status(200).json({ user, jwt_token, newAccount: false });
     } catch (err) {
         return res.status((err.isJoi && 400) || 500).json(err);
     }
